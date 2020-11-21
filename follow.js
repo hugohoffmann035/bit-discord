b8fb53281b27f412c7e90f9b35522b3055d396a6
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2020-11-21 03:44:50 -0300

    😎 Adicionado scroll automatico para carregar mais pessoas

--- follow.js
+++ follow.js
@@ -1,5 +1,6 @@
 document.querySelectorAll('.member-3-YXUe').forEach((item, idx) => {
     setTimeout(() => {
+		document.querySelector('.membersWrap-2h-GB4 > div').scrollTo(0, document.querySelector('.membersWrap-2h-GB4').scrollHeight + idx * 3)
 		item.click();
 		document.querySelector('.layer-v9HyYc > div > div > div > div > div > div').click()
 		if(document.querySelector('[role="dialog"] > div > div > header > button > div').textContent.trim().toLowerCase() !== 'enviar mensagem') {
