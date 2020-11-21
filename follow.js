6ac07f2d9beb752e78972d3e36adffcfad191a79
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2020-11-21 03:47:42 -0300

    😏 Adicionei aqui um log de quantas pessoas foram adicionada(s)

--- follow.js
+++ follow.js
@@ -1,3 +1,5 @@
+let contador = 0;
+
 document.querySelectorAll('.member-3-YXUe').forEach((item, idx) => {
     setTimeout(() => {
 		document.querySelector('.membersWrap-2h-GB4 > div').scrollTo(0, document.querySelector('.membersWrap-2h-GB4').scrollHeight + idx * 3)
@@ -5,6 +7,8 @@ document.querySelectorAll('.member-3-YXUe').forEach((item, idx) => {
 		document.querySelector('.layer-v9HyYc > div > div > div > div > div > div').click()
 		if(document.querySelector('[role="dialog"] > div > div > header > button > div').textContent.trim().toLowerCase() !== 'enviar mensagem') {
 			document.querySelector('[role="dialog"] > div > div > header > button').click()
+			contador++;
+			console.log(`Adicionado ${contador} pessoa(s)!`)
 		}
 		document.querySelector('.backdrop-1wrmKB').click()
     }, idx * 12000)
